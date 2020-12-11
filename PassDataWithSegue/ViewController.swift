b11da457fd65b8deb074ecd72bc3237af9ff6599
David Svensson <davidsvson@gmail.com>
2020-12-11 10:39:41 +0100

    Initial Commit

---
+++ PassDataWithSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataWithSegue
+//
+//  Created by David Svensson on 2020-12-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
